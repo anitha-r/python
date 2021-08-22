0993ff70ef0f43a9fb213b7e90b0b168fedea214
anitha-r <r_anitha86@hotmail.com>
2021-08-21 22:09:22 -0500

    Added aggregation to the data

--- Extract_Json.py
+++ Extract_Json.py
@@ -25,10 +25,36 @@ with open('C:\\Users\\anith\\blog\\customer2.json') as json_data:
 
 frame['balance'] = frame['balance'].str.replace('$','')
 frame['balance'] = frame['balance'].str.replace(',','')
-###frame = frame.astype({'balance',np.float32 })
+frame['balance'] = frame.balance.astype(float)
 
-print(frame.dtypes)
-print(frame.describe())
+###print("start")
+###print(frame.dtypes)
+##print(frame.describe())
+#print("test")
 
+###frame.to_csv (r'C:\\Users\\anith\\blog\\customer2.csv', index = False, header=True)
 
-frame.to_csv (r'C:\\Users\\anith\\blog\\customer2.csv', index = False, header=True)
+###BalTot = frame.groupby("gender")
+###print(BalTot.describe())
+###print(BalTot["balance"].describe())
+
+###Bal_GrpAge = frame.groupby(["gender","age"])
+###print(Bal_GrpAge["balance"].describe())
+
+###print("gpAge")
+gp_Age = frame.groupby(['gender','age']).agg({'balance': ['mean', 'min', 'max']})
+###print(gp_Age)
+
+gp_Age.columns = ['age_mean', 'age_min', 'age_max']
+
+# reset index to get grouped columns back
+gp_Age = gp_Age.reset_index()
+
+###print(gp_Age.applymap(str))
+
+text_file = open("C:\\Users\\anith\\blog\\customer2.csv", "w")
+n = text_file.write('List of aggregates for the customer by gender and age\n')
+n = text_file.write(gp_Age.to_string(index=False))
+n = text_file.write('\n\n End of Report')
+
+text_file.close()
